1f0bbc1a444c2ffdc63b65fea0a8d6d9c6e0c8d3
Ian Collins <ian.collins@gmail.com>
2009-11-04 16:51:27 -0800

    moved offsets test from html to js

--- Tests/UserTests/Element/Element.Dimensions.offsets.js
+++ Tests/UserTests/Element/Element.Dimensions.offsets.js
@@ -3,7 +3,18 @@
 		{
 			title: "Element.offsets",
 			description: "Tests the offset logic for elements.",
-			verify: "When you scroll does offset value remain fixed in place?"
+			verify: "When you scroll does the number (offset value) stay the same?",
+			before: function(){
+        window.addEvent('domready', function(){
+          var fun = function(pos_style){ $(pos_style).getFirst().set('html', $(pos_style).getOffsets().y); };
+          ["fixed", "static", "absolute"].each(function(pos_style){
+            fun(pos_style);
+            window.addEvent('scroll', function(){
+              fun(pos_style);
+            });
+          });
+        });
+    	}
 		}
 	],
 	otherScripts: ['DomReady']
