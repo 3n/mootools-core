7b567ec52f5f003b9cca76073671e012a9ae6935
Ian Collins <ian.collins@gmail.com>
2009-11-06 14:53:11 -0800

    internal scrolling area for Element.Dimensions.offsets test

--- Tests/UserTests/Element/Element.Dimensions.offsets.js
+++ Tests/UserTests/Element/Element.Dimensions.offsets.js
@@ -6,11 +6,19 @@
 			verify: "When you scroll does the number (offset value) stay the same?",
 			before: function(){
         window.addEvent('domready', function(){
+          var set_internals = function(){
+            $('internal-scroll').getChildren('div').each(function(internal){
+              internal.getFirst().set('html', internal.getOffsets().y);
+            });
+          };   
+          $('internal-scroll').addEvent('scroll', set_internals);
+          
           var fun = function(pos_style){ $(pos_style).getFirst().set('html', $(pos_style).getOffsets().y); };
           ["fixed", "static", "absolute"].each(function(pos_style){
             fun(pos_style);
             window.addEvent('scroll', function(){
               fun(pos_style);
+              set_internals();
             });
           });
         });
